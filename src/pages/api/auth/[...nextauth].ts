dd1ad719b87b6186186cc194f8ac515c50cc31a4
Mahesh Bhadane <108877616+MaheshBhadane@users.noreply.github.com>
2022-12-29 13:34:43 +0530

    solved & reverted changes

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -8,7 +8,7 @@ import { prisma } from "../../../server/db/client";
 
 export const authOptions: NextAuthOptions = {
   // Include user.id on session
-  callbacks: {
+  "callbacks": {
     session({ session, user }) {
       if (session.user) {
         session.user.id = user.id;
@@ -17,15 +17,15 @@ export const authOptions: NextAuthOptions = {
     },
   },
   // Configure one or more authentication providers
-  adapter: PrismaAdapter(prisma),
-  providers: [
+  "adapter": PrismaAdapter(prisma),
+  "providers": [
     GoogleProvider({
-      clientId: env.GOOGLE_CLIENT_ID,
-      clientSecret: env.GOOGLE_CLIENT_SECRET,
+      "clientId": env.GOOGLE_CLIENT_ID,
+      "clientSecret": env.GOOGLE_CLIENT_SECRET,
     }),
     DiscordProvider({
-      clientId: env.DISCORD_CLIENT_ID,
-      clientSecret: env.DISCORD_CLIENT_SECRET,
+      "clientId": env.DISCORD_CLIENT_ID,
+      "clientSecret": env.DISCORD_CLIENT_SECRET,
     }),
     // ...add more providers here
   ],
--- src/pages/api/examples.ts
+++ src/pages/api/examples.ts
@@ -2,6 +2,7 @@ import { type NextApiRequest, type NextApiResponse } from "next";
 
 import { prisma } from "../../server/db/client";
 
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 const examples = async (req: NextApiRequest, res: NextApiResponse) => {
   const examples = await prisma.example.findMany();
   res.status(200).json(examples);
